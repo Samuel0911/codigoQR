dfcc367f3254781a356782d024cf0053b7849363
matrixreload1 <jorge_ramirez_3000@hotmail.com>
2016-05-13 20:28:16 -0400

    corrigiendo la creacion de un usuario por default

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -12,27 +12,20 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->truncate();//esto evita que elimine y se vuelva a crear datos duplicados
-        //se debe llamar por consola "php artisan db:seed"
+        //se debe llamar por consola "php artisan db:seed"        
         factory(App\User::class)->create([
         	'name' => 'Samuel',
         	'lastName' => 'Vargas',
         	'email' => 'infosam@gmail.com',
-        	'password' => '123',
+        	'password' => bcrypt('secret'),
         	'role' => 'admin',
         	'date' => date($format = 'Y-m-d'),
         	'start_date' => date($format = 'Y-m-d'),
         	'end_date' => date($format = 'Y-m-d'),
         	'imageQR' => 'la imagen Qr es aqui',
         	'codigo_pin' => '1234'
-
-
-
-
-
-
-
     	]);
         factory(App\User::class, 4)->create();
-
+    
     }
 }
